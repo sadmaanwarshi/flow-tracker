83c0bd1614b2091e6eaec375c2cbbbd7fd463226
Sadmaan warshi <116273718+sadmaanwarshi@users.noreply.github.com>
2026-03-18 16:18:04 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -7,7 +7,7 @@ dotenv.config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: true,
+    rejectUnauthorized: false,
   },
   max: 10,
 });
